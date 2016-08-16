d7c7fbdff65b8a939211316bd096d03c09af5a24
Josh <josh@aluminati.net>
2016-08-16 12:30:25 +0100

    added testing

---
+++ client/modules/table/components/tests/table.js
@@ -0,0 +1,19 @@
+/**
+ * Created by josh.welham on 16/08/16.
+ */
+
+const {describe, it} = global;
+import React from 'react';
+import {expect} from 'chai';
+import {mount} from 'enzyme';
+import Table from './../table';
+
+describe('table.components.table', () => {
+  it('Display table content', () => {
+    const el = mount(
+      <Table />
+    );
+
+    expect(el.find('Table').length).to.equal(1);
+  });
+});
