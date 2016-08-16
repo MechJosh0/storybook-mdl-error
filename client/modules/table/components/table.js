69d3a6c283b1b2ac149b633bea62c6bac7da088c
Josh <josh@aluminati.net>
2016-08-16 12:30:45 +0100

    import mdl instead of using helmet

--- client/modules/table/components/table.js
+++ client/modules/table/components/table.js
@@ -5,6 +5,8 @@
 import React from 'react';
 import {DataTable, TableHeader} from 'react-mdl';
 import Helmet from 'react-helmet';
+import './../../../../public/lib/getmdl/material.min.js';
+import './../../../../public/lib/getmdl/material.min.css';
 
 const rows = [
   {material: 'Acrylic (Transparent)', quantity: 25, price: 2.90},
@@ -14,20 +16,15 @@ const rows = [
 
 const Table = () => (
   <div>
-    1
     <Helmet
       link={[
-        {rel: 'stylesheet', href: '/lib/getmdl/material.min.css'},
         {rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'}
-      ]}
-      script={[
-        {src: '/lib/getmdl/material.min.js', type: 'text/javascript'}
       ]}
     />
     <DataTable shadow={0} rows={rows}>
       <TableHeader name="material" tooltip="The amazing material name">Material</TableHeader>
       <TableHeader numeric name="quantity" tooltip="Number of materials">Quantity</TableHeader>
-      <TableHeader numeric name="price" cellFormatter={(price) => `\$${price.toFixed(2)}`} tooltip="Price pet unit">Price</TableHeader>
+      <TableHeader numeric name="price" tooltip="Price pet unit">Price</TableHeader>
     </DataTable>
   </div>
 );
